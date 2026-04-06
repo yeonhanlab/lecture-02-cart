3435d620df6e4936d29d30cc7e85ceb5c7f9d6a1
yeonhan <yeonhanlab@gmail.com>
2026-04-06 17:55:24 +0900

    핸들러(onAdd, onChange, onUpdateCount) 완성

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,45 @@ function App() {
     const [name, setName] = useState(""); // input 관리용 state
     const [cart, setCart] = useState([]); // 장바구니 관리용 state
 
+    const onAdd = event => {
+        event.preventDefault();
+
+        if (name === "") return;
+        setCart([...cart, { name: name, quantity: 1 }]);
+        setName("");
+    };
+
+    const onChange = e => {
+        setName(e.target.value);
+    };
+
+    const onUpdateCount = (index, number) => {
+        // 1가지 기능을 하는 함수를 만드는건데,
+        // '1을 더하는' 함수가 아니라, '수량을 변경하는' 함수를 만든 것
+        // 얼마를 바꿀 것인가는 함수를 실행할 때 매개변수를 통해 제어함
+
+        // 계산을 위해서
+        const newCart = [...cart];
+        const nextCount = newCart[index].quantity + number;
+        if (nextCount > 0) {
+            // 저장은 if문 안에서
+            newCart[index].quantity = nextCount;
+            setCart(newCart);
+        }
+    };
     return (
         <div>
             <h2>🛒 Simple Shop</h2>
             <fieldset>
                 <legend>상품 추가</legend>
-                <form>
-                    <input placeholder={"상품명을 입력하세요"} />
+                <form
+                    // (event) => {   }
+                    onSubmit={onAdd}>
+                    <input
+                        placeholder={"상품명을 입력하세요"}
+                        onChange={onChange}
+                        value={name}
+                    />
                     <button type={"submit"}>카트에 담기</button>
                 </form>
             </fieldset>
@@ -30,14 +62,33 @@ function App() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    {cart.map((value, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{value.name}</td>
+                                <td>
+                                    <button onClick={() => onUpdateCount(index, -1)}>-</button>
+                                    {value.quantity}
+                                    <button onClick={() => onUpdateCount(index, 1)}>+</button>
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => {
+                                            setCart(
+                                                cart.filter((v, i) => {
+                                                    return i !== index;
+                                                }),
+                                            );
+                                        }}>
+                                        삭제
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
-            <h3>총 품목 : 0개 / 총 수량 : 0개</h3>
+            <h3>총 품목 : {cart.length}개 / 총 수량 : 0개</h3>
         </div>
     );
 }
